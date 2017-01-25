5c177953b6dfd0ca7cc4fdbc3f724044dcb9081a
paulfelce <paulfelce@gmail.com>
2017-01-25 22:13:14 +0000

    Refactored

--- pairs.js
+++ pairs.js
@@ -1,151 +1,35 @@
 var flip1=""
-var card1_state = 0;
-var card2_state = 0;
-var card3_state = 0;
-var card4_state = 0;
-
-var card5_state = 0;
-var card6_state = 0;
-var card7_state = 0;
-var card8_state = 0;
-
-$(function()
- {
-
-$('#js-flip-1').click(
-function(){
-
-  switch(card1_state){
-    case 0:
-       $('#js-flip-1 .card').addClass('flipped');
-        card1_state=1;
-      break;
-    case 1:
-        $('#js-flip-1 .card').removeClass('flipped');
-        card1_state=0;
-      break;
-              }  
-}
-)
+var card_state=[];
+card_state["js-flip-1"]=0;
+card_state["js-flip-2"]=0;
+card_state["js-flip-3"]=0;
+card_state["js-flip-4"]=0;
+card_state["js-flip-5"]=0;
+card_state["js-flip-6"]=0;
+card_state["js-flip-7"]=0;
+card_state["js-flip-8"]=0;
 
 
-$('#js-flip-2').click(
-function(){
-  switch(card2_state){
-    case 0:
-       
-       $('#js-flip-2 .card').addClass('flipped');
-        card2_state=1;
-      break;
-    case 1:
-        $('#js-flip-2 .card').removeClass('flipped');
-        card2_state=0;
-      break;
-              }  
-}
-)
-
-$('#js-flip-3').click(
+$('.flip').click(
 function(){
   
-  switch(card3_state){
-    case 0:
-       
-       $('#js-flip-3 .card').addClass('flipped');
+  var this_card=$(this).attr('id')
   
-      if(flip1==$(this).attr('id'))
-        {
-          card3_state=2;
-          card1_state=2;          
-        }
-      card3_state=1;
+  switch(card_state[this_card]){
+    case 0:      
+       $('#' + this_card + ' .card' ).addClass('flipped');
+    
+      if(flip1==this_card)
+      {
+        
+      }
+      
+      card_state[this_card]=1;
       break;
     case 1:
-        $('#js-flip-3 .card').removeClass('flipped');
-        card3_state=0;
+        $('#' + this_card + ' .card' ).removeClass('flipped');
+        card_state[this_card]=0;
       break;
               }  
 }
 )
-
-$('#js-flip-4').click(
-function(){
-  switch(card4_state){
-    case 0:
-       
-       $('#js-flip-4 .card').addClass('flipped');
-        card4_state=1;
-      break;
-    case 1:
-        $('#js-flip-4 .card').removeClass('flipped');
-        card4_state=0;
-      break;
-              }  
-}
-)
-
-$('#js-flip-5').click(
-function(){
-  switch(card5_state){
-    case 0:
-       
-       $('#js-flip-5 .card').addClass('flipped');
-        card5_state=1;
-      break;
-    case 1:
-        $('#js-flip-5 .card').removeClass('flipped');
-        card5_state=0;
-      break;
-              }  
-}
-)
-
-$('#js-flip-6').click(
-function(){
-  switch(card6_state){
-    case 0:
-       
-       $('#js-flip-6 .card').addClass('flipped');
-        card6_state=1;
-      break;
-    case 1:
-        $('#js-flip-6 .card').removeClass('flipped');
-        card6_state=0;
-      break;
-              }  
-}
-)
-
-$('#js-flip-7').click(
-function(){
-  switch(card7_state){
-    case 0:
-       
-       $('#js-flip-7 .card').addClass('flipped');
-        card7_state=1;
-      break;
-    case 1:
-        $('#js-flip-7 .card').removeClass('flipped');
-        card7_state=0;
-      break;
-              }  
-}
-)
-
-$('#js-flip-8').click(
-function(){
-  switch(card8_state){
-    case 0:
-       
-       $('#js-flip-8 .card').addClass('flipped');
-        card8_state=1;
-      break;
-    case 1:
-        $('#js-flip-8 .card').removeClass('flipped');
-        card8_state=0;
-      break;
-              }  
-}
-)
-
-});
