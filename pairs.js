5afb19d00e2ab7f9094d806f8de166f5aef130d1
paulfelce <paulfelce@gmail.com>
2017-01-26 08:43:58 +0000

    Game completion
    Added code to count pairs and show success message when all pairs found.

--- pairs.js
+++ pairs.js
@@ -1,8 +1,9 @@
 var flip1=""
-
-
+var pairs=0;
+var test=5;
 
 $(function(){
+  
 $('.flip').click(
 function(){
  // console.log($(this).attr('princess'))
@@ -16,18 +17,35 @@ function(){
   }
   else //assume for now we haven't reclicked a shown card
   {
-      if(this_card!=flip1 && $('#' +this_card).attr('princess') ==$('#' +flip1).attr('princess')){          
+      //need to compare this_card to flip1 to check not clicking same card twice
+      if(this_card!=flip1 && $('#' +this_card).attr('princess') ==$('#' +flip1).attr('princess')){          //match    
         $('#' + this_card).removeClass('flip')  //simple way to stop re-clicking
-        $('#' + flip1).removeClass('flip')  //simple way to stop re-clicking
-        flip1=""        
+        $('#' + flip1).removeClass('flip')  //simple way to stop re-clicking      
+        
+        $('#' + this_card).find(".face.front").hide(); //hide the face front
+        $('#' + flip1).find(".face.front").hide(); //hide the face front
+        
+        flip1=""
+        pairs++;
+        
+        if(pairs==4)//found them all
+          {
+            $("#pairs_table").hide();
+            $("#finished").show();
+          }
       }
-    else{
+    else{//nomatch
       
       $('#' + this_card + ' .card').toggleClass('flipped')        
       
-    setTimeout(function(){      
-      $('.flip .card').removeClass('flipped') ;       
-      }, 1000);          
+    setTimeout(function(){
+      console.log(flip1)
+      $('.flip .card').removeClass('flipped') ; 
+      
+      
+      }, 800);
+      
+      
       
       
       flip1=""
