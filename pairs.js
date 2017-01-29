e7f70a6eaa8198e850f471c040e40721f23d3af6
paulfelce <paulfelce@gmail.com>
2017-01-29 19:12:34 +0000

    Refactored and Tested
    Old refactored version didn't take into account document ready brackets!

--- pairs.js
+++ pairs.js
@@ -4,49 +4,49 @@ var test=5;
 
 $(function(){
   
-$('.flip').click(
-function(){
-  handle_flip();
-  
+$('.flip').click(function(){
+  handle_click($(this));
+})
 });
 
+function handle_click(card){
+ // console.log($(this).attr('princess'))
+  var this_card=$(card).attr('id')
   
-//refactored so we can cleanly add a test and not run this code on already paired cards.
-function handle_flip()
-{
-  // console.log($(this).attr('princess'))
-  var this_card=$(this).attr('id')
-  
-  if(flip1=="")
-  {    
-      $('#' + this_card + ' .card').toggleClass('flipped')  
-      flip1=this_card
-  }//first card
+  if(flip1==""){
+    console.log("first card")
+    $('#' + this_card + ' .card').toggleClass('flipped')  
+    flip1=this_card
+    
+  }
   else //assume for now we haven't reclicked a shown card
   {
       //need to compare this_card to flip1 to check not clicking same card twice
-      if(this_card!=flip1 && $('#' +this_card).attr('princess') ==$('#' +flip1).attr('princess'))
-      {          //match    
-          $('#' + flip1).removeClass('flip')  //simple way to stop re-clicking      
-         // $('#' + flip1).attr('paired') = 'true'      
-          $('#' + this_card).find(".face.front").hide(); //hide the face front
-          $('#' + flip1).find(".face.front").hide(); //hide the face front
-          flip1=""
-          pairs++;
-          if(pairs==4)//found them all
+      if(this_card!=flip1 && $('#' +this_card).attr('princess') ==$('#' +flip1).attr('princess')){          //match    
+        $('#' + this_card).removeClass('flip')  //simple way to stop re-clicking
+        $('#' + flip1).removeClass('flip')  //simple way to stop re-clicking      
+        
+        $('#' + this_card).find(".face.front").hide(); //hide the face front
+        $('#' + flip1).find(".face.front").hide(); //hide the face front
+        
+        flip1=""
+        pairs++;
+        
+        if(pairs==4)//found them all
           {
             $("#pairs_table").hide();
             $("#finished").show();
-          }//if pairs
-      }//same card flip
-   else{//nomatch
-        $('#' + this_card + ' .card').toggleClass('flipped')        
-        setTimeout(function()
-        {
-          $('.flip .card').removeClass('flipped') ; 
-        }, 800);
-      flip1=""
+          }
       }
+    else{//nomatch
+      
+      $('#' + this_card + ' .card').toggleClass('flipped')        
+      
+    setTimeout(function(){
+      console.log(flip1)
+      $('.flip .card').removeClass('flipped') ;    
+      }, 800);
+      flip1=""
+    }
   }
- }
-});
+}
