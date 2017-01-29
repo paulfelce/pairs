7934ff75cf24e9c0c482cff79b22f682274e5116
paulfelce <paulfelce@gmail.com>
2017-01-29 21:33:42 +0000

    Hefty rewrite.

--- pairs.js
+++ pairs.js
@@ -1,52 +1,56 @@
 var flip1=""
 var pairs=0;
-var test=5;
+var test=7;
 
 $(function(){
   
-$('.flip').click(function(){
+$('td').click(function(){
+  if(!card_already_flipped(this))
   handle_click($(this));
 })
 });
 
-function handle_click(card){
- // console.log($(this).attr('princess'))
-  var this_card=$(card).attr('id')
-  
-  if(flip1==""){
-    console.log("first card")
-    $('#' + this_card + ' .card').toggleClass('flipped')  
-    flip1=this_card
-    
-  }
-  else //assume for now we haven't reclicked a shown card
-  {
-      //need to compare this_card to flip1 to check not clicking same card twice
-      if(this_card!=flip1 && $('#' +this_card).attr('princess') ==$('#' +flip1).attr('princess')){          //match    
-        $('#' + this_card).removeClass('flip')  //simple way to stop re-clicking
-        $('#' + flip1).removeClass('flip')  //simple way to stop re-clicking      
-        
-        $('#' + this_card).find(".face.front").hide(); //hide the face front
-        $('#' + flip1).find(".face.front").hide(); //hide the face front
-        
-        flip1=""
-        pairs++;
-        
-        if(pairs==4)//found them all
-          {
-            $("#pairs_table").hide();
-            $("#finished").show();
-          }
-      }
-    else{//nomatch
-      
-      $('#' + this_card + ' .card').toggleClass('flipped')        
-      
-    setTimeout(function(){
-      console.log(flip1)
-      $('.flip .card').removeClass('flipped') ;    
-      }, 800);
-      flip1=""
+//determine if the card is already flipped
+function card_already_flipped(obj){
+  if(($('#'+$(obj).attr('id')+' .card').hasClass('flipped')))
+    {
+      return true;
     }
+  else
+    {
+      return false;
+    }
+  
+}
+
+function handle_click(card){
+  var this_card='#' + $(card).attr('id')
+  $(this_card + ' .card').addClass('flipped')   
+  if(flip1=="")
+    {
+      flip1=this_card;
+    }
+  else  //2nd card so look for match
+  {
+   if($(this_card).attr('princess')==$(flip1).attr('princess'))
+     {
+       pairs++;
+       flip1=""
+       console.log(pairs)
+       if(pairs==4){
+         $('#pairs_table').hide();
+         $('#finished').show();
+       }
+     }//match
+    else
+      {
+        setTimeout(function(){
+        $(this_card + ' .card').removeClass('flipped')   
+        $(flip1 + ' .card').removeClass('flipped') 
+        flip1="";
+        },800);
+          
+      }//no match
+    
   }
 }
