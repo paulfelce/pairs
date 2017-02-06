f5b2f0a8160b56591a07586f535ae7b411ea1d42
paulfelce <paulfelce@gmail.com>
2017-02-06 12:44:03 +0000

    dynamically set first card
    Need to test before adding others.

--- pairs.js
+++ pairs.js
@@ -4,10 +4,13 @@ var test=7;
 
 $(function(){
   
+assign_cards(); 
+ 
 $('td').click(function(){
   if(!card_already_flipped(this))
   handle_click($(this));
 })
+  
 });
 
 //determine if the card is already flipped
@@ -22,6 +25,36 @@ function card_already_flipped(obj){
     }
   
 }
+
+//change this to reorganise the cards.
+//Ultimately want it to randomise
+function assign_cards(){
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+  $('#js-flip-1').attr('princess','pirate.png');
+  $('#img1').attr('src','pirate.png');
+  
+}
+
 
 function handle_click(card){
   var this_card='#' + $(card).attr('id')
